b95e51cf8dd0d62d5ece3d3bd24bd4f7bcbfc63b
Storulv <jordycorvisiercontact@gmail.com>
2019-12-17 10:57:49 +0100

    ajout de la page principale + edition css de celle-ci

--- homepage.php
+++ homepage.php
@@ -21,10 +21,23 @@
     <!-- Les acteurs -->
 
     <fieldset>
-        
-        <img id="logoFormation_co" src="img/formation_co.png.png" alt="logo formation_co"/>
-        <p>Formation&co est une association française présente sur tout le territoire.
-        Nous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé.</p>
+        <img class="LogoActeurs" src="img/formation_co.png" alt="logo formation_co"/>
+        <p class="pActeurs">Formation&co est une association française présente sur tout le territoire. Nous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé.</p>
+    </fieldset>
+
+    <fieldset>
+        <img class="LogoActeurs" src="img/protectpeople.png" alt="logo protectpeople"/>
+        <p class="pActeurs">Protectpeople finance la solidarité nationale. Nous appliquons le principe édifié par la Sécurité sociale française en 1945 : permettre à chacun de bénéficier d’une protection sociale.</p>
+    </fieldset>
+
+    <fieldset>
+        <img class="LogoActeurs" src="img/Dsa_france.png" alt="logo Dsa_france"/>
+        <p class="pActeurs">Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales. Nous accompagnons les entreprises dans les étapes clés de leur évolution.</p>
+    </fieldset>
+
+    <fieldset>
+        <img class="LogoActeurs" src="img/CDE.png" alt="logo CDE"/>
+        <p class="pActeurs">La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation.</p>
     </fieldset>
 
 </body>
