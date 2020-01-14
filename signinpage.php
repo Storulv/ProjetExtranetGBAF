3cc7b953a7352808c957bb3a1fa4e0652354dae7
Storulv <jordycorvisiercontact@gmail.com>
2020-01-14 19:46:36 +0100

    rassemblement des pages de log en une + PHP

--- signinpage.php
+++ signinpage.php
@@ -1,3 +1,21 @@
+<?php
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=extranet gbaf;charset=utf8', 'root', '');
+
+if(isset($_POST['form_inscription'])){
+  
+  $pseudo_insc = htmlspecialchars($_POST['pseudo_insc']);
+  $nom_insc = htmlspecialchars($_POST['nom_insc']);
+  $prenom_insc = htmlspecialchars($_POST['prenom_insc']);
+  $mdp_insc = password_hash($_POST['mdp_insc'],PASSWORD_DEFAULT);
+
+  if(!empty($_POST['pseudo_insc']) AND !empty($_POST['nom_insc']) AND !empty($_POST['prenom_insc']) AND !empty($_POST['mdp_insc'])){
+  }
+  else{
+    $erreur_insc = "Tout les champs doivent être complétés";
+  }
+}
+?>
+
 <!doctype html>
 <html lang="fr">
 <head>
@@ -20,14 +38,17 @@
 
   <!-- Formulaire de connection -->
 
-  <form method="post" action="traitement.php">
+  <form method="post" action="">
     <p>
       <fieldset>
-        <legend>Vos identifiants</legend>
+        <legend>Connexion</legend>
         
         <label for="Pseudo"> Pseudo : </label><input type="text" name="Pseudo">
         
         <label> Mot de passe : </label><input type="password" name="Mot de passe">
+
+        <input type="submit" name="form_connexion"></input>
+
       </fieldset>
     </p>
 
@@ -35,36 +56,48 @@
 
   <!-- Formulaire d'inscription -->
 
-  <form method="post" action="traitement.php">
+  <form method="post" action="">
     <p>
       <fieldset>
         <legend>Inscription</legend>
         
         <label> Nom : </label>
-        <input type="text" name="Nom">
+        <input type="text" name="nom_insc">
       
         <label> Prénom : </label>
-        <input type="text" name="Prénom">
+        <input type="text" name="prenom_insc">
         
         <label for="Pseudo"> Pseudo : </label>
-        <input type="text" name="Pseudo">
+        <input type="text" name="pseudo_insc">
         
         <label> Mot de passe : </label>
-        <input type="password" name="Mot de passe">
+        <input type="password" name="mdp_insc">
+
+        <input type="submit" name="form_inscription"></input>
       </fieldset>
     
   </form>
 
-  <!-- Formulaire d'inscription -->
+  <?php 
+  
+  if(isset($erreur_insc)){
+    echo '<font color="red">'.$erreur_insc.'</font>';
+  }
+  
+  ?>
 
-  <form method="post" action="traitement.php">
+  <!-- Formulaire de récupération -->
+
+  <form method="post" action="">
     <p>
       <fieldset>
-        <legend>Récupération de compte</legend>
+        <legend>Identifiant ou mot de passe oublié</legend>
 
         <label> Question secrète : </label><input type="text" name="Question secrète">
       
         <label> Réponse à la question secrète : </label><input type="text" name="Réponse à la question secrète">
+
+        <input type="submit" name="form_recup"></input>
       </fieldset>
     </p>
 
